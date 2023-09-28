05a9a059a9d5aaf5fe072da26dc7caca247ea162
Little Grey <51894967+djc-Sherlock@users.noreply.github.com>
2023-09-28 15:53:03 +0800

    Update APPheji_Revenuecat.js

--- Script/APPheji_Revenuecat.js
+++ Script/APPheji_Revenuecat.js
@@ -1,61 +1,55 @@
 /*
 
-Revenuecat解锁合集，已解锁APP及下载地址请见说明：https://github.com/a88/Script/blob/main/Readme.md
+Revenuecat解锁合集，已解锁APP及下载地址请见说明：https://github.com/Guding88/Script/blob/main/Readme.md
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/djc-Sherlock/loonscript/main/Script/APPheji_Revenuecat.js
-//^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://raw.githubusercontent.com/djc-Sherlock/loonscript/main/Script/APPheji_Revenuecat.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/APPheji_Revenuecat.js
+//^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://raw.githubusercontent.com/Guding88/Script/main/APPheji_Revenuecat.js
 
 [MITM]
 hostname = api.revenuecat.com
 
 */
-const a = {};
-var a6 = JSON.parse(typeof $response != "undefined" && $response.body || null);
-a6 = {
-  "request_date_ms" : 1691745938403,
-  "request_date" : "2023-03-11T09:25:38Z",
-  "subscriber" : {
-    "non_subscriptions" : {
-
-    },
-    "first_seen" : "2023-03-11T09:15:51Z",
-    "original_application_version" : "2",
-    "other_purchases" : {
-
-    },
-    "management_url" : "https://apps.apple.com/account/subscriptions",
-    "subscriptions" : {
-      "eticket_family_sharing_for_old_member" : {
-        "original_purchase_date" : "2023-03-11T09:18:00Z",
-        "expires_date" : "9999-08-18T09:18:00Z",
-        "is_sandbox" : false,
-        "refunded_at" : null,
-        "store_transaction_id" : "430001519739141",
-        "unsubscribe_detected_at" : null,
-        "grace_period_expires_date" : null,
-        "period_type" : "trial",
-        "purchase_date" : "2023-08-11T09:18:00Z",
-        "billing_issues_detected_at" : null,
-        "ownership_type" : "PURCHASED",
-        "store" : "app_store",
-        "auto_resume_date" : null
-      }
-    },
-    "entitlements" : {
-      "vip" : {
-        "grace_period_expires_date" : null,
-        "purchase_date" : "2023-03-11T09:18:00Z",
-        "product_identifier" : "eticket_family_sharing_for_old_member",
-        "expires_date" : "9999-08-18T09:18:00Z"
-      }
-    },
-    "original_purchase_date" : "2023-03-11T09:14:59Z",
-    "original_app_user_id" : "$RCAnonymousID:13a81f8ed4e7439d9b826ac61dafba31",
-    "last_seen" : "2023-03-11T09:15:51Z"
+const guding = {};
+const guding6 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+if (typeof $response == "undefined") {
+  delete $request.headers["x-revenuecat-etag"];
+  delete $request.headers["X-RevenueCat-ETag"];
+  guding.headers = $request.headers;
+} else if (guding6 && guding6.subscriber) {
+  guding6.subscriber.subscriptions = guding6.subscriber.subscriptions || {};
+  guding6.subscriber.entitlements = guding6.subscriber.entitlements || {};
+  var headers = {};
+  for (var key in $request.headers) {
+  const reg = /^[a-z]+$/;
+  if (key === "User-Agent" && !reg.test(key)) {
+    var lowerkey = key.toLowerCase();
+    $request.headers[lowerkey] = $request.headers[key];
+    delete $request.headers[key];
+    }
   }
+  var UA = $request.headers['user-agent'];
+  const app = 'gd';
+  const UAMappings = {
+    '%E8%BD%A6%E7%A5%A8%E7%A5%A8':{ name: 'vip', id: 'eticket_with_watch_life_b'},
+  };
+  const data = {
+    "expires_date": "6666-06-06T06:06:06Z",
+    "original_purchase_date": "2023-09-06T06:06:06Z",
+    "purchase_date": "2023-09-06T06:06:06Z",
+    "ownership_type": "PURCHASED",
+    "store": "app_store"
+  };
+  for (const i in UAMappings) {
+    if (new RegExp(`^${i}`, 'i').test(UA)) {
+      const { name, id } = UAMappings[i];
+      guding6.subscriber.subscriptions = {};
+      guding6.subscriber.subscriptions[id] = data;
+      guding6.subscriber.entitlements[name] = JSON.parse(JSON.stringify(data));
+      guding6.subscriber.entitlements[name].product_identifier = id;
+      break;
+    }
+  }
+  guding.body = JSON.stringify(guding6);
 }
-
-  a.body = JSON.stringify(a6);
-
-$done(a);
+$done(guding);
