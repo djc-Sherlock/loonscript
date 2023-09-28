0c80a66de6e00a7050b898802afcbee40678ca63
Little Grey <51894967+djc-Sherlock@users.noreply.github.com>
2023-09-28 15:43:46 +0800

    Update APPheji_Revenuecat.js

--- Script/APPheji_Revenuecat.js
+++ Script/APPheji_Revenuecat.js
@@ -14,12 +14,12 @@ const a = {};
 const a6 = JSON.parse(typeof $response != "undefined" && $response.body || null);
 a6 = {
   "request_date_ms" : 1691745938403,
-  "request_date" : "2023-08-11T09:25:38Z",
+  "request_date" : "2023-03-11T09:25:38Z",
   "subscriber" : {
     "non_subscriptions" : {
 
     },
-    "first_seen" : "2023-08-11T09:15:51Z",
+    "first_seen" : "2023-03-11T09:15:51Z",
     "original_application_version" : "2",
     "other_purchases" : {
 
@@ -27,7 +27,7 @@ a6 = {
     "management_url" : "https://apps.apple.com/account/subscriptions",
     "subscriptions" : {
       "eticket_family_sharing_for_old_member" : {
-        "original_purchase_date" : "2023-08-11T09:18:00Z",
+        "original_purchase_date" : "2023-03-11T09:18:00Z",
         "expires_date" : "9999-08-18T09:18:00Z",
         "is_sandbox" : false,
         "refunded_at" : null,
@@ -45,14 +45,14 @@ a6 = {
     "entitlements" : {
       "vip" : {
         "grace_period_expires_date" : null,
-        "purchase_date" : "2023-08-11T09:18:00Z",
+        "purchase_date" : "2023-03-11T09:18:00Z",
         "product_identifier" : "eticket_family_sharing_for_old_member",
         "expires_date" : "9999-08-18T09:18:00Z"
       }
     },
-    "original_purchase_date" : "2023-08-11T09:14:59Z",
+    "original_purchase_date" : "2023-03-11T09:14:59Z",
     "original_app_user_id" : "$RCAnonymousID:13a81f8ed4e7439d9b826ac61dafba31",
-    "last_seen" : "2023-08-11T09:15:51Z"
+    "last_seen" : "2023-03-11T09:15:51Z"
   }
 }
 
